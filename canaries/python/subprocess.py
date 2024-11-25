b48d5be38c0ded1fdbbeecfb70d2a0caca16ee3c
Victor Barroncas <59754066+victor-nb@users.noreply.github.com>
2024-11-25 07:27:16 -0500

    Update subprocess.py

--- canaries/python/subprocess.py
+++ canaries/python/subprocess.py
@@ -2,3 +2,5 @@ import subprocess
 import sys
 
 subprocess.call("grep -R {} .".format(sys.argv[1]), shell=True)
+
+github_token = "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"
